ee91cae17f61bd58b1d140c83a8d6c56c574df62
madhuri <guravmadhuri93@gmail.com>
2024-03-07 12:14:06 +0530

    ListOfprimeNumbers

---
+++ BasicCoreJava24/src/numbers/ListOfPrimeNumbers.java
@@ -0,0 +1,30 @@
+package numbers;
+
+public class ListOfPrimeNumbers {
+
+public static void main(String[] args) {
+        //Prime number from 2 to 20
+			 for(int i=2;i<=20;i++) {
+				 
+		int count=0;
+		for(int j=2;j<=i/2;j++) 
+		{
+				if(i%j==0) {
+				     count++;
+				 	 break;
+				 			}	
+		}
+		
+				 		
+		if(count==0) 
+		{
+				 			System.out.println(i);	
+		}	
+
+			}
+
+		}
+
+	}
+
+
